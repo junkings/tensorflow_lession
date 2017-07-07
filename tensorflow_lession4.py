f7782bf15c09c4e5f1eeb5d6d2797ecd8918da38
jin <876142347@qq.com>
2017-07-07 18:58:00 +0800

    lession4 cnn

--- tensorflow_lession4.py
+++ tensorflow_lession4.py
@@ -21,7 +21,7 @@ train_step = tf.train.GradientDescentOptimizer(0.01).minimize(cross_entropy)
 
 for i in range(1000):
 	batch = mnist.train.next_batch(50)
-	train_step.run(feed_dict={x:batch[0], y_: batch[1]})
+	train_step.run(feed_dict={x: batch[0], y_: batch[1]})
 
 correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_prediction, "float"))
